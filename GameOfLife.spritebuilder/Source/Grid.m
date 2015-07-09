ff63223c08184ea9e8ce63a894da0467965ee797
apple <apple@appledeMacBook-Air.local>
2015-07-09 15:32:09 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -138,7 +138,7 @@ static const int GRID_COLUMNS = 10;
 
 -(void)updateCreatures{
     
-    int numAlive = 0;
+  //  int numAlive = 0;
     
     //loop through rows
     for (int i =0; i < [_gridArray count]; i++){
